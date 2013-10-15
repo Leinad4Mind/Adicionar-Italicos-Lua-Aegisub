da9cade7b86ce20b82a35636a7487caadea7c652
Daniel Mota <Leinad4Mind@gmail.com>
2013-10-15 13:07:45 +0100

    Nova versão adicionada!
    Esta nova versão já não coloca itálicos em linhas que já os contém.

---
+++ adicionar_italicos_v2.lua
@@ -0,0 +1,26 @@
+﻿-- Por Leinad4Mind
+--
+
+script_name = "Adicionar Itálicos"
+script_description = "Adicionar itálicos nas linhas seleccionadas"
+script_author = "Leinad4Mind"
+script_version = "2.0"
+script_modified = "15 de Outubro 2013"
+
+require("re")
+return aegisub.register_macro(script_name, script_description, function(subs, sel)
+  for _index_0 = 1, #sel do
+    local i = sel[_index_0]
+    local l = subs[i]
+    local s = l.text
+    if s and not re.match(s, "\\{[^}]*\\\\(?:i)\\d", re.ICASE) then
+      if s:match("^%s*{") then
+        l.text = s:gsub("^(%s*{)", "%1\\i1")
+      else
+        l.text = "{\\i1}" .. s .. "{\\i0}"
+      end
+      subs[i] = l
+    end
+  end
+  return sel
+end)
